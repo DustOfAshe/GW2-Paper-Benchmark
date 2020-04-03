eb046d38d2ef810f04cf2254976d27c1c5897b6b
Crigges <andy400@web.de>
2020-04-03 17:53:15 +0200

    unsaved changes

--- src/main/java/systems/rine/pb/crawler/Skill.java
+++ src/main/java/systems/rine/pb/crawler/Skill.java
@@ -10,41 +10,29 @@ public class Skill {
     public List<Fact> facts = null;
     public String description;
     @SerializedName("icon")
-    @Expose
     public String icon;
     @SerializedName("type")
-    @Expose
     public String type;
     @SerializedName("weapon_type")
-    @Expose
     public String weaponType;
     @SerializedName("professions")
-    @Expose
     public List<String> professions = null;
     @SerializedName("slot")
-    @Expose
     public String slot;
     @SerializedName("next_chain")
-    @Expose
     public Integer nextChain;
     @SerializedName("flip_skill")
-    @Expose
     public Integer flipSkill;
     @SerializedName("flags")
-    @Expose
     public List<String> flags = null;
     @SerializedName("specialization")
-    @Expose
     public Integer specialization;
     @SerializedName("id")
-    @Expose
     public Integer id;
     public String chatLink;
     @SerializedName("categories")
-    @Expose
     public List<Object> categories = null;
     @SerializedName("traited_facts")
-    @Expose
     public List<TraitedFact> traitedFacts = null;
 
 }
